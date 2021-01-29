20ed98c729bb37d6d711b164c8a87360be593b81
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-28 19:22:14 -0500

    If Else

---
+++ main.js
@@ -0,0 +1,8 @@
+  
+let edad = parseInt(prompt("Ingrese su edad: "));
+
+if(edad >= 18){
+    console.log(`Es mayor de edad`);
+}else{
+    console.log(`La edad ingresada no cumple con los parámetros`);
+}
